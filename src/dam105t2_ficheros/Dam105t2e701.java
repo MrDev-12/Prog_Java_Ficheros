ff333b91b0197e0e842ffae5a045a151bcc1a43c
MrDev-12 <micorreoelectronico258@gmail.com>
2020-03-31 16:00:35 +0200

    first commit

---
+++ src/dam105t2_ficheros/Dam105t2e701.java
@@ -0,0 +1,21 @@
+
+package dam105t2_ficheros;
+
+import java.util.Scanner;
+
+
+public class Dam105t2e701 {
+
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Introduce un dividendo entero: ");
+        int dividendo = teclado.nextInt();
+
+        System.out.println("Introduce un divisor entero: ");
+        int divisor = teclado.nextInt();
+        
+    }
+    
+}
